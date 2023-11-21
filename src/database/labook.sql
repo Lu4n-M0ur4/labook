ab0a43218c168dbcc84c0fefb5b0b54d6b5b9fb7
Lu4n-M0ur4 <luan.moura-rj@hotmail.com>
2023-11-21 16:36:58 -0300

    Ajust SQL

--- src/database/labook.sql
+++ src/database/labook.sql
@@ -1,4 +1,4 @@
--- Active: 1700503272829@@127.0.0.1@3306
+-- Active: 1700574792135@@127.0.0.1@3306
 
 CREATE TABLE
     users (
@@ -17,10 +17,10 @@ CREATE TABLE
         content TEXT NOT NULL,
         likes INTEGER NOT NULL DEFAULT(0),
         dislikes INTEGER NOT NULL DEFAULT(0),
-        created_at TEXT NOT NULL DEFAULT(CURRENT_TIMESTAMP ),
-        updated_at TEXT NOT NULL DEFAULT(CURRENT_TIMESTAMP ),
+        created_at TEXT NOT NULL,
+        updated_at TEXT NOT NULL,
         FOREIGN KEY (creator_id) 
-        REFERENCES user (id) 
+        REFERENCES users (id) 
         ON UPDATE CASCADE 
         ON DELETE CASCADE
     )
@@ -29,13 +29,12 @@ CREATE TABLE likes_dislikes(
     user_id TEXT NOT NULL,
     post_id TEXT NOT NULL,
     like INTEGER NOT NULL DEFAULT(0),
-    FOREIGN KEY ( user_id) REFERENCES users (id)
+    FOREIGN KEY (user_id) REFERENCES users (id)
         ON UPDATE CASCADE 
         ON DELETE CASCADE,
     FOREIGN KEY (post_id) REFERENCES posts (id)
         ON UPDATE CASCADE 
         ON DELETE CASCADE
-  
 )
 
 INSERT INTO users
@@ -84,4 +83,6 @@ VALUES (
 
     UPDATE posts SET dislikes = 1 WHERE id = 'p002';
 
-SELECT * FROM users
+SELECT * FROM posts;
+
+DROP Table posts;
